a01c8f7fc44f19b2965d0ea651fde61e59f3671c
Miguel Angel Gutierrez Duque <82895821+mgutierrezdu@users.noreply.github.com>
2025-07-14 18:19:53 -0500

    r_wip

--- WIP/unified_geolocator(development)/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Unified Geolocator Plugin for QGIS
-Combines image geolocation and distance calculation in one interface
-"""
-
-def classFactory(iface):
-    from .main import UnifiedGeolocatorPlugin
-    return UnifiedGeolocatorPlugin(iface) 
